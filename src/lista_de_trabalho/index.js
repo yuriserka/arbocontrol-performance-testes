383047b117b24c96ae8f7d1bab680ced4fec5e3f
Yuri Serka <yserka@hotmail.com>
2020-06-29 19:14:10 -0300

    correção de bug no registro da lista de trabalho

--- src/lista_de_trabalho/index.js
+++ src/lista_de_trabalho/index.js
@@ -1,4 +1,5 @@
 const registroJson = require('../../data/registro_lista_de_trabalho.json');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
     createRegistroJsonBody: (req, ctx, ee, next) => {
@@ -7,6 +8,7 @@ module.exports = {
             atividade: ctx.vars.atividade_criada_obj,
             formulario: ctx.vars.inspecao_geral_form_obj,
             imovel: ctx.vars.imovel_criado_obj,
+            uuid: uuidv4(),
         };
         return next();
     },
